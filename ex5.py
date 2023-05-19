111d924d91f03ad3d5eafdb80e76884dcfd8490d
nohannah <nohannah508@gmail.com>
2023-05-20 00:56:50 +0700

    not yet complete

---
+++ ex5.py
@@ -0,0 +1,7 @@
+#Write a program to ask for the user’s last name and then ask for their first name. Then, display the 
+#output message Hello [Last Name] [First Name]. Try using only one input statement
+
+#user name 
+user_name= str(input("Enter your last name:" )) 
+first_name=str(input("Enter your first name:"))
+print("Hello :", user_name + first_name)
