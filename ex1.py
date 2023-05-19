5633aac72e3fee91793b6379e36670dc599846d2
nohannah <nohannah508@gmail.com>
2023-05-20 00:02:53 +0700

    correct

--- ex1.py
+++ ex1.py
@@ -1,2 +1,10 @@
-print("Hello world")
-print("Hello world")
+#1. Asks for the radius and the depth of a cylinder and displays the total volume rounded to three decimal
+#points.
+#circle area = π * radius2
+#volume = circle area * depth
+from math import pi
+r=int(input("Enter number of radius:"))
+d=int(input("Enter dept of cylinder:"))
+cicle_area= pi * r**r 
+volume= cicle_area * d
+print(round(volume , 3))
