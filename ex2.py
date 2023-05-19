b6eebc524734105826408cc32776bb875a81496e
nohannah <nohannah508@gmail.com>
2023-05-20 00:08:59 +0700

    correct

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#2. What does each of the following statements return?
+#1). "This is Chapter 3".upper()
+#2). "welcome".capitalize() + " to " + "Chapter 3".upper()
+#3). " Welcome to Python.".strip(" .")
+print("This is Chapter 3".upper())
+print("welcome".capitalize() + " to " + "Chapter 3".upper())
+print(" Welcome to Python.".strip("."))
